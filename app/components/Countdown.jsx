b4fc4da884cf017842923e3f78f3c1970dfd150a
David Lee <peakersky@gmail.com>
2016-12-26 21:48:43 +0900

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+var Countdown = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Countdown Component
+            </div>
+        )
+    }
+});
+
+module.exports = Countdown;
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,9 @@ module.exports = {
         alias: {
             applicationStyles: 'app/styles/app.scss',
             Main: 'app/components/Main.jsx',
-            Nav: 'app/components/Nav.jsx'
+            Nav: 'app/components/Nav.jsx',
+            Countdown: 'app/components/Countdown.jsx',
+            Timer: 'app/components/Timer.jsx'
         },
         extensions: ['', '.js', '.jsx']
     },
